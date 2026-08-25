e75a8df62653450e13ac5b75fc3112de9db1600a
Scott <tobiascodes12@gmail.com>
2026-08-25 15:18:13 -0400

    Assemble full approved Aisha portrait

--- frontend/src/aishaPortraitData.js
+++ frontend/src/aishaPortraitData.js
@@ -1,5 +1,14 @@
-import approvedPortrait from "./aishaPortraitChunks/hq1.js";
+import hq01 from "./aishaPortraitChunks/hq01.js";
+import hq02 from "./aishaPortraitChunks/hq02.js";
+import hq03 from "./aishaPortraitChunks/hq03.js";
+import hq04 from "./aishaPortraitChunks/hq04.js";
+import hq05 from "./aishaPortraitChunks/hq05.js";
+import hq06 from "./aishaPortraitChunks/hq06.js";
+import hq07 from "./aishaPortraitChunks/hq07.js";
+import hq08 from "./aishaPortraitChunks/hq08.js";
+import hq09 from "./aishaPortraitChunks/hq09.js";
+import hq10 from "./aishaPortraitChunks/hq10.js";
 
 export const AISHA_PORTRAIT_WIDTH = 816;
 export const AISHA_PORTRAIT_HEIGHT = 551;
-export const AISHA_PORTRAIT_DATA_URI = `data:image/jpeg;base64,${approvedPortrait}`;
+export const AISHA_PORTRAIT_DATA_URI = `data:image/jpeg;base64,${hq01}${hq02}${hq03}${hq04}${hq05}${hq06}${hq07}${hq08}${hq09}${hq10}`;
