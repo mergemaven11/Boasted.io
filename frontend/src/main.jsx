704ce31ecc1d03a923fcabf91c79d956f7d43d0a
Scott <tobiascodes12@gmail.com>
2026-08-25 13:38:26 -0400

    Load interview scoring calibration polish

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./ProductPolish.css";
 import "./ReferencePolish.css";
 import "./MarketingFooterOrder.css";
+import "./InterviewScoringCalibration.css";
 import RootContent from "./RootContent.jsx";
 import { initializeAnalytics } from "./analytics.js";
 
