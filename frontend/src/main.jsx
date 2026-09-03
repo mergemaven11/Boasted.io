04fdde2c814d775ae9b668a75bc6a39720490825
Tobias <tobiascodes12@gmail.com>
2026-09-03 16:21:58 -0400

    fix: load mobile landing layout safeguards

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import "./ReferencePolish.css";
 import "./MarketingFooterOrder.css";
 import "./ResponsiveLayoutGuard.css";
+import "./LandingPageMobileFix.css";
 import RootContent from "./RootContent.jsx";
 import { installInterviewBrowserPreflight } from "./interviewBrowserPreflight.js";
 
