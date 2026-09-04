c469f5509a313a9b0d18479de8c6b5f1949b17d5
Tobias <tobiascodes12@gmail.com>
2026-09-04 15:48:22 -0400

    Stop analytics from loading before consent

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import "./ResponsiveLayoutGuard.css";
 import "./ImpactReceiptsResponsive.css";
 import "./VerifiedImpact.css";
 import "./ProfileUploadPolish.css";
+import AnalyticsConsentBanner from "./AnalyticsConsentBanner.jsx";
 import PublicAuthHeader from "./PublicAuthHeader.jsx";
 import RootContent from "./RootContent.jsx";
 import { installInterviewBrowserPreflight } from "./interviewBrowserPreflight.js";
@@ -19,19 +20,8 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PublicAuthHeader />
     <RootContent />
+    <AnalyticsConsentBanner />
   </StrictMode>,
 );
 
 void installPublicPortfolioAvatar();
-
-function loadAnalyticsWhenIdle() {
-  import("./analytics.js")
-    .then(({ initializeAnalytics }) => initializeAnalytics())
-    .catch(() => {});
-}
-
-if ("requestIdleCallback" in window) {
-  window.requestIdleCallback(loadAnalyticsWhenIdle, { timeout: 2500 });
-} else {
-  window.setTimeout(loadAnalyticsWhenIdle, 1500);
-}
